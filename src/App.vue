cacbcb56fef04031a9c8b5707f58dee75e13d320
andy521218 <631154511@qq.com>
2020-10-10 18:06:40 +0800

    清除缓存

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    window.onbeforeunload = function () {
-      var storage = window.localStorage;
-      storage.clear();
-    };
-  },
-};
-</script>
